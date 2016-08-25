cf782b3896af0a9f9ebb3a4785e440107e334909
managr <marcin.naglik@gmail.com>
2016-08-25 17:13:17 +0200

    Adding code climate coverage

--- Gemfile
+++ Gemfile
@@ -1,4 +1,7 @@
 source 'https://rubygems.org'
 
-# Specify your gem's dependencies in sidekiq-batch.gemspec
 gemspec
+
+group :test do
+  gem 'codeclimate-test-reporter'
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'fakeredis/rspec'
 require 'sidekiq/batch'

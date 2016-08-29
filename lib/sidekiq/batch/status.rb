6c4298c1084295bf483b8f386175975b34447b11
managr <marcin.naglik@gmail.com>
2016-08-29 22:52:34 +0200

    Adding failures implementation

--- lib/sidekiq/batch/status.rb
+++ lib/sidekiq/batch/status.rb
@@ -15,6 +15,10 @@ module Sidekiq
         Sidekiq.redis { |r| r.get("BID-#{bid}-to_process") }.to_i
       end
 
+      def failures
+        Sidekiq.redis { |r| r.scard("BID-#{bid}-failed") }.to_i
+      end
+
       def complete?
         'true' == Sidekiq.redis { |r| r.hget("BID-#{bid}", 'complete') }
       end

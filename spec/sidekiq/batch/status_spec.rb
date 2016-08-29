35b4e8f9b041869c25fbc3434e6320cbc9407172
managr <marcin.naglik@gmail.com>
2016-08-29 23:17:05 +0200

    Adding specs for Status#failures

--- spec/sidekiq/batch/status_spec.rb
+++ spec/sidekiq/batch/status_spec.rb
@@ -26,9 +26,26 @@ describe Sidekiq::Batch::Status do
     end
   end
 
+  describe '#failures' do
+    context 'when not initalized' do
+      it 'returns 0 failed jobs' do
+        expect(subject.failures).to eq(0)
+      end
+    end
+
+    context 'when more than 0' do
+      before { Sidekiq::Batch.increment_job_queue(bid) }
+      before { Sidekiq::Batch.process_failed_job(bid, 'FAILEDID') }
+
+      it 'returns failed jobs' do
+        expect(subject.failures).to eq(1)
+      end
+    end
+  end
+
   describe '#data' do
     it 'returns batch description' do
-      expect(subject.data).to eq(total: nil, failures: nil, pending: 0, created_at: nil, complete: false, failure_info: nil)
+      expect(subject.data).to eq(total: nil, failures: 0, pending: 0, created_at: nil, complete: false, failure_info: nil)
     end
   end
 end

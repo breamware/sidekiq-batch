bf366c62cd86323eac73e703c61ebad1f615e21e
managr <marcin.naglik@gmail.com>
2016-09-07 23:55:48 +0200

    Adding focus

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,3 +15,8 @@ end
 Sidekiq.configure_server do |config|
   config.redis = redis_opts
 end
+
+RSpec.configure do |config|
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+end

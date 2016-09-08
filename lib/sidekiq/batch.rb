f80a96971a4c5b79750b88676c29df3094726d7c
managr <marcin.naglik@gmail.com>
2016-09-08 22:55:58 +0200

    Set expire to 30 days (if job does not finish)

--- lib/sidekiq/batch.rb
+++ lib/sidekiq/batch.rb
@@ -10,28 +10,48 @@ module Sidekiq
   class Batch
     class NoBlockGivenError < StandardError; end
 
+    BID_EXPIRE_TTL = 108_000
+
     attr_reader :bid, :description, :callback_queue
 
     def initialize(existing_bid = nil)
       @bid = existing_bid || SecureRandom.urlsafe_base64(10)
-      Sidekiq.redis { |r| r.hset("BID-#{bid}", 'created_at', Time.now) }
+      Sidekiq.redis do |r|
+        r.multi do
+          r.hset("BID-#{bid}", 'created_at', Time.now)
+          r.expire("BID-#{bid}", BID_EXPIRE_TTL)
+        end
+      end
     end
 
     def description=(description)
       @description = description
-      Sidekiq.redis { |r| r.hset("BID-#{bid}", 'description', description) }
+      Sidekiq.redis do |r|
+        r.multi do
+          r.hset("BID-#{bid}", 'description', description)
+          r.expire("BID-#{bid}", BID_EXPIRE_TTL)
+        end
+      end
     end
 
     def callback_queue=(callback_queue)
       @callback_queue = callback_queue
-      Sidekiq.redis { |r| r.hset("BID-#{bid}", 'callback_queue', callback_queue) }
+      Sidekiq.redis do |r|
+        r.multi do
+          r.hset("BID-#{bid}", 'callback_queue', callback_queue)
+          r.expire("BID-#{bid}", BID_EXPIRE_TTL)
+        end
+      end
     end
 
     def on(event, callback, options = {})
       return unless %w(success complete).include?(event.to_s)
       Sidekiq.redis do |r|
-        r.hset("BID-#{bid}", "callback_#{event}", callback)
-        r.hset("BID-#{bid}", "callback_#{event}_opts", options.to_json)
+        r.multi do
+          r.hset("BID-#{bid}", "callback_#{event}", callback)
+          r.hset("BID-#{bid}", "callback_#{event}_opts", options.to_json)
+          r.expire("BID-#{bid}", BID_EXPIRE_TTL)
+        end
       end
     end
 
@@ -52,6 +72,7 @@ module Sidekiq
             r.sadd("BID-#{bid}-failed", jid)
             r.scard("BID-#{bid}-failed")
             r.hget("BID-#{bid}", 'to_process')
+            r.expire("BID-#{bid}-failed", BID_EXPIRE_TTL)
           end
         end
         if to_process[2].to_i == to_process[1].to_i
@@ -65,6 +86,7 @@ module Sidekiq
             r.hincrby("BID-#{bid}", 'to_process', -1)
             r.scard("BID-#{bid}-failed")
             r.hincrby("BID-#{bid}", 'pending', -1)
+            r.expire("BID-#{bid}", BID_EXPIRE_TTL)
           end
         end
 
@@ -86,6 +108,7 @@ module Sidekiq
             %w(to_process pending total).each do |c|
               r.hincrby("BID-#{bid}", c, 1)
             end
+            r.expire("BID-#{bid}", BID_EXPIRE_TTL)
           end
         end
       end

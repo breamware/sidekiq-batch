c87a27bfaa3ef2986d488f952cdc88acbe3e08e7
managr <marcin.naglik@gmail.com>
2016-12-29 14:24:11 +0100

    Fix for new codeclimate configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
+require "simplecov"
+SimpleCov.start
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'fakeredis/rspec'

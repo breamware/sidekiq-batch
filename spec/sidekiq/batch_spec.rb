9c0deafd6974d72dc1511244071849510e2c7bac
Jason Brady <jabrady42@gmail.com>
2018-12-12 18:23:42 -0600

    Rename thread local var to :batch to reflect its type. Update the worker so Worker.bid is an id and not batch object.

--- spec/sidekiq/batch_spec.rb
+++ spec/sidekiq/batch_spec.rb
@@ -70,7 +70,7 @@ describe Sidekiq::Batch do
     it 'sets Thread.current bid' do
       batch = Sidekiq::Batch.new
       batch.jobs do
-        expect(Thread.current[:bid]).to eq(batch)
+        expect(Thread.current[:batch]).to eq(batch)
       end
     end
   end

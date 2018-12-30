407df60aebde634bc49102dc6d3e8f26d65382c8
Jason <jabrady42@gmail.com>
2018-12-29 22:10:42 -0600

    Add callback batch and finalization

--- lib/sidekiq/batch/callback.rb
+++ lib/sidekiq/batch/callback.rb
@@ -14,12 +14,30 @@ module Sidekiq
             instance = object.new
             instance.send(method, status, opts) if instance.respond_to?(method)
           end
+        end
+      end
 
-          send(event.to_sym, bid, status, parent_bid)
+      class Finalize
+        def dispatch status, opts
+          bid = opts["bid"]
+          callback_bid = status.bid
+          event = opts["event"].to_sym
+          callback_batch = bid != callback_bid
+
+          Sidekiq.logger.debug {"Finalize #{event} batch id: #{opts["bid"]}, callback batch id: #{callback_bid} callback_batch #{callback_batch}"}
+
+          batch_status = Status.new bid
+          send(event, bid, batch_status, batch_status.parent_bid)
+
+          if callback_batch
+            # Different events are run in different batches
+            Sidekiq::Batch.cleanup_redis callback_bid
+          end
+          Sidekiq::Batch.cleanup_redis bid if event == :success
         end
 
-
         def success(bid, status, parent_bid)
+          Sidekiq.logger.debug {"Finalize parent success bid: #{parent_bid}"}
           if (parent_bid)
             _, _, success, _, complete, pending, children, failure = Sidekiq.redis do |r|
               r.multi do
@@ -38,10 +56,6 @@ module Sidekiq
             Batch.enqueue_callbacks(:success, parent_bid) if pending.to_i.zero? && children == success
             # if job finished successfully and parent batch completed call parent complete callback
             Batch.enqueue_callbacks(:complete, parent_bid) if complete == children && pending == failure
-          end
-
-          Sidekiq.redis do |r|
-            r.del "BID-#{bid}-success", "BID-#{bid}-complete", "BID-#{bid}-jids", "BID-#{bid}-failed"
           end
         end
 
@@ -62,6 +76,9 @@ module Sidekiq
           # We don't want to run this if the batch was successfull because the success
           # callback may add more jobs to the batch
           if parent_bid and not (pending.to_i.zero? && children == success)
+          Sidekiq.logger.debug {"Finalize parent complete bid: #{parent_bid}"}
+
+          if (parent_bid)
             _, complete, pending, children, failure = Sidekiq.redis do |r|
               r.multi do
                 r.sadd("BID-#{parent_bid}-complete", bid)
@@ -74,6 +91,10 @@ module Sidekiq
 
             Batch.enqueue_callbacks(:complete, parent_bid) if complete == children && pending == failure
           end
+        end
+        def cleanup_redis bid, callback_bid=nil
+          Sidekiq::Batch.cleanup_redis bid
+          Sidekiq::Batch.cleanup_redis callback_bid if callback_bid
         end
       end
     end

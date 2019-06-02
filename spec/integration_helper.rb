9516475be580d1a1e53ea47fcb1e2dd3c105adec
Jason Brady <jabrady42@gmail.com>
2019-06-02 08:17:45 -0500

    DRY integration tests

--- spec/integration_helper.rb
+++ spec/integration_helper.rb
@@ -8,12 +8,41 @@ end
 
 Sidekiq.redis { |r| r.flushdb }
 
-# Sidekiq.logger.level = :debug
-
 def redis_keys
   Sidekiq.redis { |r| r.keys('BID-*') }
 end
 
 def dump_redis_keys
   puts redis_keys.inspect
+end
+
+def process_tests
+  out_buf = StringIO.new
+  Sidekiq.logger = Logger.new out_buf
+
+  # Sidekiq.logger.level = :info
+
+  Sidekiq::Worker.drain_all
+
+  output = out_buf.string
+  keys = redis_keys
+  puts out_buf.string
+
+  [output, keys]
+end
+
+def overall_tests output, keys
+  describe "sidekiq batch" do
+    it "runs overall complete callback" do
+      expect(output).to include "Overall Complete"
+    end
+
+    it "runs overall success callback" do
+      expect(output).to include "Overall Success"
+    end
+
+    it "cleans redis keys" do
+      expect(keys).to eq([])
+    end
+  end
 end
